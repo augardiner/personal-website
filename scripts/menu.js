59c5052124e2294146f35b3ceae10fb9f290c78d
Alex Gardiner <agardiner07@gmail.com>
2020-05-14 19:07:25 -0400

    added mobile menu

---
+++ scripts/menu.js
@@ -0,0 +1,30 @@
+const breakpoint = window.matchMedia( "(max-width: 960px)" );
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('topbar');
+const menuAnchors = menu.querySelectorAll('a');
+
+menuButton.addEventListener('click', () => {
+  if (menu.style.display === 'none') {
+    menu.style.display = 'inline-block';
+  } else {
+    menu.style.display = 'none';
+  }
+})
+
+menuButton.addEventListener('mouseover', () => {
+  menuButton.style.cursor = "pointer";
+})
+
+menu.addEventListener('blur', () => {
+  menu.style.display = 'none';
+})
+
+menuAnchors.forEach((a) => {
+  a.addEventListener('click', () => {
+    menu.style.display = 'none';
+  })
+})
+
+menu.onblur = () => {
+  menu.style.display = 'none';
+}
